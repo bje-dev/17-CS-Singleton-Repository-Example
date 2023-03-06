4874efb8074887b9a7733b83685566e6cb6fb2f5
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2023-03-05 21:35:47 -0300

    First commit singleton repository example

---
+++ BLL/Services/AddressBusinessLogic.cs
@@ -0,0 +1,59 @@
+﻿using BLL.Contracts;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    
+
+
+    public sealed class AddressBusiness : IGenericBusinessLogic<Address>
+    {
+            private readonly static AddressBusiness _instance = new AddressBusiness();
+
+        public static AddressBusiness Current
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private AddressBusiness()
+            {
+                //Implent here the initialization of your singleton
+            }
+        
+
+
+
+        public void Add(Address obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Address> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Address GetOne(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Address obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CS Singleton Repository Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("CS Singleton Repository Example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CS Singleton Repository Example")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d73005ac-ed36-4e5a-9469-113d9326d902")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
